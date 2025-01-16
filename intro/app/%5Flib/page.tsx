c6dbac29049fcbc4cb6f66d4f55cc5c1a7da7d46
saicharanbm <saicharanbm@gmail.com>
2025-01-16 13:04:57 +0530

    Add PrivateRoute component to restrict direct access to specific routes

---
+++ intro/app/%5Flib/page.tsx
@@ -0,0 +1,9 @@
+function PrivateRoute() {
+  return (
+    <div>
+      You cannot view this route directly unless you prefic the route with %5F.
+    </div>
+  );
+}
+
+export default PrivateRoute;
